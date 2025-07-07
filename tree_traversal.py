39d6c2d439da582c5a17a6e9b739b851fce1f014
Cameron Stanley <cam96stanley@gmail.com>
2025-07-07 14:42:13 -0400

    added trees.py for binary search tree logic

---
+++ tree_traversal.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.left = None
+    self.right = None
+
+class BinarySearchTree:
+  def __init__(self):
+    self.root = None
+  
+  def breadth_first_traversal(self):
+    if not self.root:
+      raise Exception("Tree is empty")
+    queue = deque(self.root)
+    queue.append(self.root)
+    visited = []
+    while queue:
+      visited_node = queue.popleft()
+      visited.append(visited_node.data)
+      if visited_node.left:
+        queue.append(visited_node.left)
+      if visited_node.right:
+        queue.append(visited_node.right)
+    return visited
