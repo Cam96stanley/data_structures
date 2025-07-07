b042d22127f34dce0f334cd136ba6ab7adf7ddd1
Cameron Stanley <cam96stanley@gmail.com>
2025-07-07 16:31:38 -0400

    created tree traversal logic

--- tree_traversal.py
+++ tree_traversal.py
@@ -25,3 +25,99 @@ class BinarySearchTree:
       if visited_node.right:
         queue.append(visited_node.right)
     return visited
+  
+  def dft_pre_order_iterative(self):
+    if not self.root:
+      raise Exception("Tree is empty")
+    stack = [self.root]
+    visited = []
+    while stack:
+      visited_node = stack.pop()
+      visited.append(visited_node.data)
+      if visited_node.left:
+        stack.append(visited_node.left)
+      if visited_node.right:
+        stack.append(visited_node.right)
+    return visited
+  
+  def dft_pre_order_recursion(self):
+    if not self.root:
+      raise Exception("Tree is empty")
+    visited = []
+    
+    def _traverse(node):
+      if node:
+        visited.append(node.data)
+        _traverse(node.left)
+        _traverse(node.right)
+      return
+    
+    _traverse(self.root)
+    return visited
+  
+  def dft_in_order_iterative(self):
+    if not self.root:
+      raise Exception("Tree is empty")
+    current_node = self.root
+    stack = []
+    visited = []
+    while stack or current_node:
+      if current_node:
+        stack.append(current_node)
+        current_node = current_node.left
+      else:
+        visited_node = stack.pop()
+        visited.append(visited_node)
+        if not visited_node.right:
+          continue
+        current_node = visited_node.right
+    return visited
+  
+  def dft_in_order_recursion(self):
+    if not self.root:
+      raise Exception("Tree is empty")
+    visited = []
+    
+    def _traverse(node):
+      if node:
+        _traverse(node.left)
+        visited.append(node.data)
+        _traverse(node.right)
+      return
+    
+    _traverse(self.root)
+    return visited
+  
+  def dft_post_order_iterative(self):
+    if not self.root:
+      raise Exception("Tree is empty")
+    current = previous = self.root
+    stack = []
+    visited = []
+    while current:
+      while current.left:
+        stack.append(current)
+        current = current.left
+        while not current.right or current.right == previous:
+          visited.appen(current.data)
+          previous = current
+          if not stack:
+            return visited
+          current = stack.pop()
+        stack.append(current)
+        current = current.right
+  
+  def dft_post_order_recursion(self):
+    if not self.root:
+      raise Exception("Tree is empty")
+    visited = []
+    
+    def _traverse(node):
+      if node:
+        _traverse(node.left)
+        _traverse(node.right)
+        visited.append(node.data)
+      return
+    
+    _traverse(self.root)
+    return visited
